3478777980eef5171d821eb8a0df21a27e296f6c
Dave Kludt <oldarmyc@gmail.com>
2019-07-09 08:39:49 -0500

    Add setup.py file to get things installed

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import setuptools
+import sys
+
+
+requirements = ['sh', 'pyyaml']
+test_requirements = ['mock', 'nose', 'flake8']
+
+
+setuptools.setup(
+    name='accord',
+    version='0.1.0',
+    url='https://github.com/oldarmyc/accord',
+    license='Apache License, Version 2.0',
+    author='Dave Kludt',
+    author_email='dkludt@anaconda.com',
+    description=(
+        'Backup and restore Anaconda Enterprise 5'
+    ),
+    zip_safe=False,
+    platforms='any',
+    install_requires=requirements,
+    extras_require={
+        'tests': test_requirements
+    },
+    entry_points={
+        'console_scripts': [
+            'accord=accord.process:main'
+        ]
+    },
+    packages=['accord'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
